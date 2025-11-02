40b9efa72bbf658f4ece8a6efbbeee6cc77c7f16
reo <sodapon03@gmail.com>
2025-11-02 17:11:21 +0900

    Initial Commit

---
+++ BMI/BMIApp.swift
@@ -0,0 +1,17 @@
+//
+//  BMIApp.swift
+//  BMI
+//
+//  Created by 田口怜央 on 2025/11/02.
+//
+
+import SwiftUI
+
+@main
+struct BMIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
