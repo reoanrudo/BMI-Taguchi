ad0bc36223f030c1d7f1341d17b9f4e86c1edda7
reo <sodapon03@gmail.com>
2025-11-02 18:29:29 +0900

    数字入力ができた

--- BMI/ContentView.swift
+++ BMI/ContentView.swift
@@ -8,11 +8,33 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var enterdNumber: Double = 0.0 // 1: TextFieldに入力した数値の変数
+    @State private var originalUnit = "meter"
+    private var convertedNumber: Double = 0 // 換算後の数値。
+    
+    let heightUnits = ["meter", "centimeter"] //身長の単位
+    
     var body: some View {
         Form {
+            Section("入力") {
+                HStack{
+                    TextField("Original", value: $enterdNumber, format: .number)
+                        .keyboardType(.numberPad)
+                        .padding()
+                    
+                    //Picker("", selection: $heightUnits){
+                    //ForEach(heightUnits, id: \.self) { unit in
+                    //Text(unit)
+                }
+            }
             
+
+            
+            Section("結果") {
+                Text("Text 1")
+            }
         }
-        
     }
 }
 
