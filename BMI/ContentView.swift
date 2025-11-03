9c63b72b889c61a4b05ddbe57c6d1baaa7fcdbf6
reo <sodapon03@gmail.com>
2025-11-03 14:04:11 +0900

    少数第二まで表現できた、小数点ありのdecimalpadにした、あとは評価

--- BMI/ContentView.swift
+++ BMI/ContentView.swift
@@ -9,22 +9,48 @@ import SwiftUI
 
 struct ContentView: View {
     
+    // MARK: - 変数保存
+    
     @State private var height: Double = 0.0
     @State private var weight: Double = 0.0
     @State private var unit = "meter"
-    
-    var bmi: Double {
-        weight / (height * height)
-    }
+    @FocusState private var isFocused: Bool
     
     let units = ["meter", "centimeter"]
+    
+    // MARK: - BMI計算式
+    
+    var bmi: Double {
+        switch unit {
+        case "centimeter":
+            return weight / ((height/100) * (height/100))
+        default:
+            return weight / (height * height)
+        }
+    }
+    
+    
+    // MARK: - Body
     
     var body: some View {
         Form {
             Section("入力") {
                 HStack{
                     TextField("", value: $height, format: .number)
-                        .keyboardType(.numberPad)
+                        .keyboardType(.decimalPad)
+                        .focused($isFocused)
+                        .toolbar {
+                            ToolbarItem(placement: .keyboard) {
+                                HStack{
+                                    Spacer()
+                                    Button {
+                                        isFocused = false
+                                    } label: {
+                                        Text("Done")
+                                    }
+                                }
+                            }
+                        }
                     
                     Picker("", selection: $unit){
                         ForEach(units, id: \.self) { unit in
@@ -35,19 +61,21 @@ struct ContentView: View {
                 
                 HStack{
                     TextField("", value: $weight, format: .number)
-                        .keyboardType(.numberPad)
+                        .keyboardType(.decimalPad)
+                        .focused($isFocused)
                     
                     Text("kg")
                 }
             }
             
             Section("結果") {
-                Text("\(bmi)")
+                Text("\(bmi, specifier: "%.2f")")
             }
         }
         
     }
 }
+
 #Preview {
     ContentView()
 }
